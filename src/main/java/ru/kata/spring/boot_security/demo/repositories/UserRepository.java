1e6880190c1e9cb900b576c3c287c2d6a7762eaf
reduss21 <kartosferon@gmail.com>
2023-02-18 02:09:28 +0300

    LAZY war

--- src/main/java/ru/kata/spring/boot_security/demo/repositories/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repositories/UserRepository.java
@@ -2,14 +2,15 @@ package ru.kata.spring.boot_security.demo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.kata.spring.boot_security.demo.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query("select u from User u where u.username = ?1")
-    User findByUsername(String username);
+    @Query("select u from User u join fetch u.roles where u.username = (:username)")
+    User findByUsername(@Param("username") String username);
 
     @Query("select u from User u where u.id = ?1")
     User findUserById(Long id);
