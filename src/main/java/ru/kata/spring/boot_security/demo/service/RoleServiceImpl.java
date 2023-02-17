45817a31d89db735bc46f2e938d5a04672538c3c
reduss21 <kartosferon@gmail.com>
2023-02-17 19:30:02 +0300

    fixes

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
@@ -0,0 +1,25 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.models.Role;
+import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
+
+import java.util.List;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Transactional
+    @Override
+    public List<Role> allRoles() {
+        return roleRepository.findAll();
+    }
+}
